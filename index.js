6bfb3c5933bc373ed0cb47b61f0ffd846bcded49
Martin <martingarcia804@yahoo.com>
2023-03-01 22:39:09 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(object) {
+        return document.body.innerHTML = object.id
+    })
+    .catch(function(error) {
+        alert("Error! Please try again.")
+        return document.body.innerHTML = error.message
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   })
+//   .catch(function (error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   });
